cac2402ed15706c4b0a4f1d96748628afe70118b
Matvey1338 <duo163@yandex.ru>
2024-12-09 20:07:15 +0400

    new

--- main.cpp
+++ main.cpp
@@ -4,16 +4,179 @@ using namespace std;
 
 int main()
 {
-	vector<int> vec1{ 1,2,3,5,4,6,7,8,9,10,11,12 };
-	vector<int> vec2{1,2,3,5,4,6,7,8,9,10,11,12};
-	stats<int> res;
-	stats<int> res2;
+    std::srand(std::time(nullptr));
 
-	res = BublleSort(vec1);
-	res2 = CombSort(vec2);
+    std::vector<size_t> sizes = { 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 25000, 50000, 100000 };
+    const size_t max_num = 100;
+    /*
+    std::ofstream file("C:/Users/matve/OneDrive/Рабочий стол/project_current_semestr/aisd/lab3/sort_results/bubble_sort_results.csv");
+    file << "Size,Avg Comparisons,Avg Copies,Sorted Comparisons,Sorted Copies,Reverse Comparisons,Reverse Copies\n";
+    std::cout << "BUBBLE:" << "\n";
+    std::cout << "----------------------------------" << std::endl;
 
-	cout << res.comparison_count << " and " << res.copy_count << endl;
-	cout << res2.comparison_count << " and " << res2.copy_count << endl;
+ 
+    for (size_t size : sizes)
+    {
+        long long total_comparisons = 0;
+        long long total_copies = 0;
+
+        for (size_t i = 0; i < max_num; ++i)
+        {
+            std::vector<int> arr(size);
+            generator(arr, size, i);
+
+            stats result = BublleSort(arr);
+
+            total_comparisons += result.comparison_count;
+            total_copies += result.copy_count;
+        }
+
+        long long avg_comparisons = total_comparisons / max_num;
+        long long avg_copies = total_copies / max_num;
+
+        std::cout << "Size: " << size << std::endl;
+        std::cout << "Average comparisons: " << avg_comparisons << std::endl;
+        std::cout << "Average copies: " << avg_copies << std::endl;
+
+        std::vector<int> sorted_arr(size);
+        generateSortedArray(sorted_arr, size);
+        stats sorted_result = BublleSort(sorted_arr);
+        std::cout << "Comparisons (sorted): " << sorted_result.comparison_count << std::endl;
+        std::cout << "Copies (sorted): " << sorted_result.copy_count << std::endl;
+
+        std::vector<int> reverse_sorted_arr(size);
+        generateReverseSortedArray(reverse_sorted_arr, size);
+        stats reverse_sorted_result = BublleSort(reverse_sorted_arr);
+        std::cout << "Comparisons (reverse sorted): " << reverse_sorted_result.comparison_count << std::endl;
+        std::cout << "Copies (reverse sorted): " << reverse_sorted_result.copy_count << std::endl;
+
+        std::cout << "----------------------------------" << std::endl;
+
+        file << size << ","
+            << avg_comparisons << ","
+            << avg_copies << ","
+            << sorted_result.comparison_count << ","
+            << sorted_result.copy_count << ","
+            << reverse_sorted_result.comparison_count << ","
+            << reverse_sorted_result.copy_count << "\n";
+    }
+    file.close();
+    std::cout << "Results saved to bubble_sort_results.csv" << std::endl;
+    */
+
+    /*
+    std::ofstream file("C:/Users/matve/OneDrive/Рабочий стол/project_current_semestr/aisd/lab3/sort_results/comb_sort_results.csv");
+    file << "Size,Avg Comparisons,Avg Copies,Sorted Comparisons,Sorted Copies,Reverse Comparisons,Reverse Copies\n";
+
+    std::cout << "COMB:" << "\n";
+    std::cout << "----------------------------------" << std::endl;
+
+
+    for (size_t size : sizes)
+    {
+        long long total_comparisons = 0;
+        long long total_copies = 0;
+
+        for (size_t i = 0; i < max_num; ++i)
+        {
+            std::vector<int> arr(size);
+            generator(arr, size, i);
+
+            stats result = CombSort(arr);
+
+            total_comparisons += result.comparison_count;
+            total_copies += result.copy_count;
+        }
+
+        long long avg_comparisons = total_comparisons / max_num;
+        long long avg_copies = total_copies / max_num;
+
+        std::cout << "Size: " << size << std::endl;
+        std::cout << "Average comparisons: " << avg_comparisons << std::endl;
+        std::cout << "Average copies: " << avg_copies << std::endl;
+
+        std::vector<int> sorted_arr(size);
+        generateSortedArray(sorted_arr, size);
+        stats sorted_result = CombSort(sorted_arr);
+        std::cout << "Comparisons (sorted): " << sorted_result.comparison_count << std::endl;
+        std::cout << "Copies (sorted): " << sorted_result.copy_count << std::endl;
+
+        std::vector<int> reverse_sorted_arr(size);
+        generateReverseSortedArray(reverse_sorted_arr, size);
+        stats reverse_sorted_result = CombSort(reverse_sorted_arr);
+        std::cout << "Comparisons (reverse sorted): " << reverse_sorted_result.comparison_count << std::endl;
+        std::cout << "Copies (reverse sorted): " << reverse_sorted_result.copy_count << std::endl;
+
+        std::cout << "----------------------------------" << std::endl;
+
+        file << size << ","
+            << avg_comparisons << ","
+            << avg_copies << ","
+            << sorted_result.comparison_count << ","
+            << sorted_result.copy_count << ","
+            << reverse_sorted_result.comparison_count << ","
+            << reverse_sorted_result.copy_count << "\n";
+    }
+    file.close();
+    std::cout << "Results saved to comb_sort_results.csv" << std::endl;
+    */
+
+    /*
+    std::ofstream file("C:/Users/matve/OneDrive/Рабочий стол/project_current_semestr/aisd/lab3/sort_results/pyramid_sort_results.csv");
+    file << "Size,Avg Comparisons,Avg Copies,Sorted Comparisons,Sorted Copies,Reverse Comparisons,Reverse Copies\n";
+
+    std::cout << "PYRAMID:" << "\n";
+    std::cout << "----------------------------------" << std::endl;
+
+
+    for (size_t size : sizes)
+    {
+        long long total_comparisons = 0;
+        long long total_copies = 0;
+
+        for (size_t i = 0; i < max_num; ++i)
+        {
+            std::vector<int> arr(size);
+            generator(arr, size, i);
+
+            stats result = heapSort(arr);
+
+            total_comparisons += result.comparison_count;
+            total_copies += result.copy_count;
+        }
+
+        long long avg_comparisons = total_comparisons / max_num;
+        long long avg_copies = total_copies / max_num;
+
+        std::cout << "Size: " << size << std::endl;
+        std::cout << "Average comparisons: " << avg_comparisons << std::endl;
+        std::cout << "Average copies: " << avg_copies << std::endl;
+
+        std::vector<int> sorted_arr(size);
+        generateSortedArray(sorted_arr, size);
+        stats sorted_result = heapSort(sorted_arr);
+        std::cout << "Comparisons (sorted): " << sorted_result.comparison_count << std::endl;
+        std::cout << "Copies (sorted): " << sorted_result.copy_count << std::endl;
+
+        std::vector<int> reverse_sorted_arr(size);
+        generateReverseSortedArray(reverse_sorted_arr, size);
+        stats reverse_sorted_result = heapSort(reverse_sorted_arr);
+        std::cout << "Comparisons (reverse sorted): " << reverse_sorted_result.comparison_count << std::endl;
+        std::cout << "Copies (reverse sorted): " << reverse_sorted_result.copy_count << std::endl;
+
+        std::cout << "----------------------------------" << std::endl;
+
+        file << size << ","
+            << avg_comparisons << ","
+            << avg_copies << ","
+            << sorted_result.comparison_count << ","
+            << sorted_result.copy_count << ","
+            << reverse_sorted_result.comparison_count << ","
+            << reverse_sorted_result.copy_count << "\n";
+    }
+    file.close();
+    std::cout << "Results saved to pyramid_sort_results.csv" << std::endl;
+    */
 
 	return 0;
 }
--- sorting.h
+++ sorting.h
@@ -1,6 +1,8 @@
 ﻿#pragma once
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <ctime>
 
 
 using namespace std;
@@ -11,6 +13,12 @@ struct stats {
     size_t copy_count = 0;
 
     stats() = default;
+
+    stats& operator+=(const stats a) {
+        this->comparison_count += a.comparison_count;
+        this->copy_count += a.copy_count;
+        return *this;
+    }
 };
 
 template <typename T>
@@ -61,4 +69,94 @@ stats<T> CombSort(vector<T>& vector) {
     }
 
     return res;
+}
+
+template <typename T>
+stats<T> heapify(vector<T>& arr, int n, int i) {
+    int largest = i;          // Инициализируем корень как самый большой
+    int left = 2 * i + 1;     // Левый дочерний элемент
+    int right = 2 * i + 2;    // Правый дочерний элемент
+    stats<T> res{};
+
+    // Если левый дочерний элемент больше корня
+    res.comparison_count++;
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+        res.copy_count++;
+    }
+
+    // Если правый дочерний элемент больше самого большого элемента
+    res.comparison_count++;
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+        res.copy_count++;
+    }
+    // Если самый большой элемент не корень
+    res.comparison_count++;
+    if (largest != i) {
+        swap(arr[i], arr[largest]); // Меняем местами
+        res.copy_count += 3;
+
+        // Рекурсивно преобразуем поддерево в кучу
+        res = heapify(arr, n, largest);
+    }
+    
+    return res;
+}
+
+// Основная функция пирамидальной сортировки
+template <typename T>
+stats<T> heapSort(vector<T>& arr) {
+    int n = arr.size();
+    stats<T> res{};
+    stats<T> merge_res{};
+
+    // Построение кучи (перестройка массива)
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        merge_res = heapify(arr, n, i);
+        res += merge_res;
+
+    }
+
+    // Один за другим извлекаем элементы из кучи
+    for (int i = n - 1; i > 0; i--) {
+        // Перемещаем текущий корень в конец
+        swap(arr[0], arr[i]);
+        res.copy_count += 3;
+
+
+        // Вызываем heapify на уменьшенной куче
+        merge_res = heapify(arr, i, 0);
+        res += merge_res;
+    }
+
+    return res;
+}
+
+void printArray(const vector<int>& arr) {
+    for (int val : arr)
+        cout << val << " ";
+    cout << endl;
+}
+
+void generator(std::vector<int>& arr, size_t size, unsigned int seed)
+{
+    std::srand(seed);
+    for (size_t i = 0; i < size; ++i) {
+        arr[i] = rand() % 100000;
+    }
+}
+
+void generateSortedArray(std::vector<int>& arr, size_t size)
+{
+    for (size_t i = 0; i < size; ++i) {
+        arr[i] = i;
+    }
+}
+
+void generateReverseSortedArray(std::vector<int>& arr, size_t size)
+{
+    for (size_t i = 0; i < size; ++i) {
+        arr[i] = size - i - 1;
+    }
 }
